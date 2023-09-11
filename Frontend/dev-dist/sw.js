754ab335114f2332dce3eaac6fa185bb37a77d23
정재범 <zxsefv@gmail.com>
2023-09-11 17:56:03 +0900

    :sparkles: [FE]Feat: 대출현황 컴포넌트 생성

--- Frontend/dev-dist/sw.js
+++ Frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.48f77inbun"
+    "revision": "0.3h3coc27hu"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- Frontend/src/pages/Child/Loan/Loan.tsx
+++ Frontend/src/pages/Child/Loan/Loan.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import Credit from '../../../components/Credit';
+import LoanStatus from '../../../components/Loan/LoanStatus';
+import { Link } from 'react-router-dom';
 
 const Loan: React.FC = () => {
     return (
-        <div>
-
-        <h2>자식 대출 페이지</h2>
+        <div className='mt-20'>
+            <h2 className="fixed inset-x-0 top-10 z-50 left-0">자식 대출 페이지</h2>
+            <Credit></Credit>
+            <div className='flex flex-col justify-center border border-2'>
+            <LoanStatus></LoanStatus>
+            <button className='font-bold p-2 pt-1 pb-1 ml-6 mr-6' style={{color:'#363636', backgroundColor:'#ABC3D0'}}>대출 신청</button>
+            </div>
+            <p className='text-xs'>지난 대출 보기</p>
+            <Link to='/LoanCompleted'>
+            <hr className='border-1' style={{borderColor: '#ABD0CE'}}/>
+            </Link>
         </div>
     )
 };
