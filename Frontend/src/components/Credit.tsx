35d659b300f67f1fc62939f50aeff2a50077ce2c
정재범 <zxsefv@gmail.com>
2023-09-11 14:45:31 +0900

    :art: [FE]Feat: 예금페이지 css 수정

--- Frontend/src/components/Credit.tsx
+++ Frontend/src/components/Credit.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import GaugeChart from 'react-gauge-chart'
+import GaugeChart from 'react-gauge-chart';
 
 const Credit: React.FC = () => {
-    const credit = 1
+    const credit = 5
     const creditNumber = (credit >= 1 && credit <= 10) ? (0.05 + (credit - 1) * 0.1) : 0.95;
     return (
-        <div className="border">
+        <div className="border rounded-md mr-6 ml-6 mb-4">
         <h2>신용 정보</h2>
         <GaugeChart id="gauge-chart1" 
   nrOfLevels={10}
@@ -13,8 +13,8 @@ const Credit: React.FC = () => {
   textColor={'#000000'}
   hideText={true}
 />
-        <h2>신용등급: {credit}</h2>
-        <h2>알바횟수: </h2>
+        <h2 className='text-2xl font-bold'>신용등급: {credit}</h2>
+        <h2 className='text-xl'>알바횟수: 2/3</h2>
         </div>
     )
 };
--- Frontend/src/pages/Child/Deposit/Deposit.tsx
+++ Frontend/src/pages/Child/Deposit/Deposit.tsx
@@ -3,25 +3,111 @@ import Credit from '../../../components/Credit';
 import { Link } from 'react-router-dom';
 
 const Deposit: React.FC = () => {
+    const DepositName = '뀨뀨 정기예금'
+    const interestrate = 10;
+    const interest = '복리'
+    const maturity = '2023.10.06'
+    const credit = '3'
     return (
-        <div>
+        <div className='mt-20'>
         <h2 className="fixed inset-x-0 top-10 z-50 left-0">자식 예금</h2>
         <div>
         <Credit></Credit>
         </div>
-        <div className='border'>n등급을 위한 예금 금리 ~%</div>
-
-        <div>가입한 예금 상품
-            <Link to='/DepositDetail'>
-            <div>예금 상품1</div>
-            </Link>
-            <div>예금 상품1</div>
-        </div>
-        <Link to="/DepositJoinDetail">
-            <div className=' rounded-md shadow-md m-4'>
-                <p className='text-lg'>예금 가입하기+</p>
+        <div className='border rounded-md m-6'>
+            <p className='text-lg'>
+            {credit}등급을 위한 예금 금리 
+            </p>
+            <p className='text-2xl font-bold'>
+            {'1.5 ~ 2'}%
+            </p>
             </div>
+
+        <div>
+            <div className='text-2xl font-bold mb-3'>
+            가입한 예금 상품
+            </div>
+            <Link to='/DepositDetail'>
+            <div className='border rounded-md m-6 mb-0 flex justify-around'>
+                <div className='m-3'>
+                    <p className='text-black'>{DepositName}</p>
+                    <div className='flex justify-around'>
+                    <p className={interestrate < 0 ? 'text-blue-500' : 'text-red-500'}>{interestrate}%</p>
+                    <p className='text-black'>{interest}</p>
+                    </div>
+                </div>
+                <div className='m-3'>
+                    <p className='text-black'>만기일</p>
+                    <p className='text-black'>{maturity}</p>
+                </div>
+            </div>
+            </Link>
+            <Link to='/DepositDetail'>
+            <div className='border rounded-md m-6 mb-0 flex justify-around'>
+                <div className='m-3'>
+                    <p className='text-black'>{DepositName}</p>
+                    <div className='flex justify-around'>
+                    <p className={interestrate < 0 ? 'text-blue-500' : 'text-red-500'}>{interestrate}%</p>
+                    <p className='text-black'>{interest}</p>
+                    </div>
+                </div>
+                <div className='m-3'>
+                    <p className='text-black'>만기일</p>
+                    <p className='text-black'>{maturity}</p>
+                </div>
+            </div>
+            </Link>
+            <Link to='/DepositDetail'>
+            <div className='border rounded-md m-6 mb-0 flex justify-around'>
+                <div className='m-3'>
+                    <p className='text-black'>{DepositName}</p>
+                    <div className='flex justify-around'>
+                    <p className={interestrate < 0 ? 'text-blue-500' : 'text-red-500'}>{interestrate}%</p>
+                    <p className='text-black'>{interest}</p>
+                    </div>
+                </div>
+                <div className='m-3'>
+                    <p className='text-black'>만기일</p>
+                    <p className='text-black'>{maturity}</p>
+                </div>
+            </div>
+            </Link>
+            <Link to='/DepositDetail'>
+            <div className='border rounded-md m-6 mb-0 flex justify-around'>
+                <div className='m-3'>
+                    <p className='text-black'>{DepositName}</p>
+                    <div className='flex justify-around'>
+                    <p className={interestrate < 0 ? 'text-blue-500' : 'text-red-500'}>{interestrate}%</p>
+                    <p className='text-black'>{interest}</p>
+                    </div>
+                </div>
+                <div className='m-3'>
+                    <p className='text-black'>만기일</p>
+                    <p className='text-black'>{maturity}</p>
+                </div>
+            </div>
+            </Link>
+            {/* {deposits.map((deposit, index) => (
+            <div key={index} className='border rounded-md m-6 flex justify-around'>
+                <div className='m-3'>
+                    <p className='text-black'>{deposit.DepositName}</p>
+                    <div className='flex justify-around'>
+                    <p className={deposit.interestrate < 0 ? 'text-blue-500' : 'text-red-500'}>{deposit.interestrate}</p>
+                    <p className='text-black'>{deposit.interest}</p>
+                    </div>
+                </div>
+                <div className='m-3'>
+                    <p className='text-black'>만기일</p>
+                    <p className='text-black'>{deposit.maturity}</p>
+                </div>
+            </div>
+            ))} */}
+        </div>
+            <div className='rounded-md shadow-md fixed bottom-10 left-[10%] w-[80%] bg-gray-200'>
+        <Link to="/DepositJoinDetail">
+                <p className='text-lg'>예금 가입하기+</p>
         </Link>
+            </div>
         </div>
     )
 };
