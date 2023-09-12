8b0b09a9cea1075c764efd61ef867626b7a911df
hsj4436 <hsj4436@gmail.com>
2023-09-12 15:03:09 +0900

    :sparkles: [BE]Feat: BaseEntity와 User entity 구현

---
+++ Backend/src/main/java/com/ssafy/teentech/common/entity/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.ssafy.teentech.common.entity;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class BaseEntity {
+    @Column(name = "CREATED_AT")
+    @NotNull
+    private LocalDateTime createdAt;
+
+    @Column(name = "MODIFIED_AT")
+    @NotNull
+    private LocalDateTime modifiedAt;
+}
