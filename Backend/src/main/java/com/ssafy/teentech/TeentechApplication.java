1eda8d3e426f6dbcac23a8117d48f108f7c04418
hsj4436 <hsj4436@gmail.com>
2023-09-12 14:46:32 +0900

    :sparkles: [BE]Feat: JpaAuditing 어노테이션 추가

--- Backend/src/main/java/com/ssafy/teentech/TeentechApplication.java
+++ Backend/src/main/java/com/ssafy/teentech/TeentechApplication.java
@@ -2,8 +2,10 @@ package com.ssafy.teentech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TeentechApplication {
 
 	public static void main(String[] args) {
