902d31ad6f28d4e1cece2a5a3b5d45ba4ccdf15c
un2642 <un2642@naver.com>
2023-09-12 17:42:06 +0900

    💄 [FE]Design : lotto페이지

---
+++ Frontend/src/components/Lotto/LottoList.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LottoList: React.FC = () => {
+  return (
+    <div>
+      <div>당첨내역이요..</div>
+      <div>당첨되면 좋겠다..</div>
+    </div>
+  );
+};
+
+export default LottoList;
--- Frontend/src/pages/Child/Lotto/Lotto.tsx
+++ Frontend/src/pages/Child/Lotto/Lotto.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import LottoList from "../../../components/Lotto/LottoList";
 
 const Lotto: React.FC = () => {
-    return (
-        <div>
-
-        <h2>자식 복권 페이지</h2>
+  return (
+    <div className="pt-16">
+      <div className="flex justify-center">
+        <div className="w-1/2 ml-4 mr-2" style={{ backgroundColor: "pink" }}>
+          <div className="my-2">복권 교환권</div>
+          <div className="my-2">2장</div>
         </div>
-    )
+        <div className="w-1/2 mr-4 ml-2" style={{ backgroundColor: "skyblue" }}>
+          <div className="my-2">당첨금</div>
+          <div className="my-2">5,000원</div>
+        </div>
+      </div>
+      <div>
+        <LottoList />
+      </div>
+      <Link to="/LottoChange">복권 바꾸기</Link>
+      <h2>자식 복권 페이지</h2>
+    </div>
+  );
 };
 
 export default Lotto;
