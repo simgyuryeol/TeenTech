392695fbd27089e4714dacac5c46ea2aa65fc7ed
정재범 <zxsefv@gmail.com>
2023-09-15 14:25:15 +0900

    :construction_worker: [FE]Chore: 빌드파일 변경

--- Frontend/dev-dist/sw.js
+++ Frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.8duogvlkje8"
+    "revision": "0.bm5v6ibmp6"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
