5f9142353dcdf9a101bbec6758ff29c218133988
hsj4436 <hsj4436@gmail.com>
2023-09-18 16:21:37 +0900

    :tada: [BE]Init: Initial Commit

---
+++ Bank/src/main/java/com/ssafy/teentech/TeentechApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.teentech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeentechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeentechApplication.class, args);
+	}
+
+}
