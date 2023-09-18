b59a429ed2134e58aefb6cdc0ccc07448dc39aa8
yoon-b <eunbi.kim.kor@gmail.com>
2023-09-18 11:25:26 +0900

    🎨 [FE] Style: 파일명 및 코드 수정

--- Frontend/src/firebase.ts
+++ Frontend/src/firebase.ts
@@ -1,22 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging";
 
-const {
-	VITE_FIREBASE_API_KEY,
-	VITE_FIREBASE_AUTH_DOMAIN,
-	VITE_FIREBASE_PROJECT_ID,
-	VITE_FIREBASE_STORAGE_BUCKET,
-	VITE_FIREBASE_SENDER_ID,
-	VITE_FIREBASE_APP_ID,
-} = import.meta.env
-
 const firebaseConfig = {
-	apiKey: VITE_FIREBASE_API_KEY,
-	authDomain: VITE_FIREBASE_AUTH_DOMAIN,
-	projectId: VITE_FIREBASE_PROJECT_ID,
-	storageBucket: VITE_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: VITE_FIREBASE_SENDER_ID,
-	appId: VITE_FIREBASE_APP_ID,
+	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID,
+	appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const firebase = initializeApp(firebaseConfig);
