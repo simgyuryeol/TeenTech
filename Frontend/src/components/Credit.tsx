9181a27489294b277f0e7b7a7473945709785b7d
yoon-b <eunbi.kim.kor@gmail.com>
2023-09-20 17:00:34 +0900

    💄 [FE]Design: 신용등급 차트 색 변경 옵션 추가

--- Frontend/src/components/Credit.tsx
+++ Frontend/src/components/Credit.tsx
@@ -2,21 +2,27 @@ import React from "react";
 import GaugeChart from "react-gauge-chart";
 
 const Credit: React.FC = () => {
-    const credit = 5
-    const creditNumber = (credit >= 1 && credit <= 10) ? (0.05 + (credit - 1) * 0.1) : 0.95;
-    return (
-        <div className="border border-2 rounded-xl mr-6 ml-6 mb-4 bg-white" style={{borderColor: '#ABD0CE'}}>
-        <h2>신용 정보</h2>
-        <GaugeChart id="gauge-chart1" 
-  nrOfLevels={10}
-  percent={creditNumber}
-  textColor={'#000000'}
-  hideText={true}
-/>
-        <h2 className='text-2xl font-bold'>신용등급: {credit}</h2>
-        <h2 className='text-xl mb-1'>알바횟수: 2/3</h2>
-        </div>
-    )
+  const credit = 5;
+  const creditNumber =
+    credit >= 1 && credit <= 10 ? 0.05 + (credit - 1) * 0.1 : 0.95;
+  return (
+    <div
+      className="border-2 rounded-xl mr-6 ml-6 mb-4 bg-white"
+      style={{ borderColor: "#ABD0CE" }}
+    >
+      <h2>신용 정보</h2>
+      <GaugeChart
+        id="gauge-chart1"
+        nrOfLevels={10}
+        percent={creditNumber}
+        colors={["#FFC371", "#FF5F6D"]} 
+        textColor={"#000000"}
+        hideText={true}
+      />
+      <h2 className="text-2xl font-bold">신용등급: {credit}</h2>
+      <h2 className="text-xl mb-1">알바횟수: 2/3</h2>
+    </div>
+  );
 };
 
 export default Credit;
