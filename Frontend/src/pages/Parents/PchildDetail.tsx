a2a5c24bb5ab203392b5372c4242420f5edf68f9
un2642 <un2642@naver.com>
2023-09-25 09:53:27 +0900

    🐛 [FE]Bug : 부모자녀상세 id 타입에러해결

--- Frontend/src/pages/Parents/PchildDetail.tsx
+++ Frontend/src/pages/Parents/PchildDetail.tsx
@@ -32,6 +32,7 @@ const Data: Child[] = [
 const PchildDetail: React.FC = () => {
   const [childData, setChildData] = useRecoilState(childIdAtom);
   const { id } = useParams<{ id: string }>();
+  let childId = Number(id) + 1; // OK
   const [isPaneOpen, setIsPaneOpen] = useState(false);
 
   useEffect(() => {
@@ -66,30 +67,27 @@ const PchildDetail: React.FC = () => {
         ))}
       </div>
       <div
-          className="bg-white m-3 rounded-xl mb-4 drop-shadow-xl "
-          style={{backgroundColor:'#2E8CFA'}}
+        className="bg-white m-3 rounded-xl mb-4 drop-shadow-xl "
+        style={{ backgroundColor: "#2E8CFA" }}
       >
         <div className="flex items-center justify-between">
-          <div
-            className="text-xl text-start p-3 pl-5 text-white"
-          >
-            {Data[id-1].name} 남은 용돈
-        </div>
+          <div className="text-xl text-start p-3 pl-5 text-white">
+            {Data[childId].name} 남은 용돈
+          </div>
           <div className="mr-3" onClick={() => setIsPaneOpen(true)}>
-            <img src="../../../src/assets/pmain/ellipsis.png" /></div>
+            <img src="../../../src/assets/pmain/ellipsis.png" />
           </div>
-          <div
-            className="text-5xl text-center p-3 pl-5 text-white"
-          >
-            100000
-          </div>
-          <div>
-            {/* <MenuList /> */}
         </div>
+        <div className="text-5xl text-center p-3 pl-5 text-white">100000</div>
+        <div>{/* <MenuList /> */}</div>
         <div className="flex justify-center text-white pb-2">
-          <Link to={'/Paccountbook'} className="mx-2 text-white">가계부</Link>
+          <Link to={"/Paccountbook"} className="mx-2 text-white">
+            가계부
+          </Link>
           <div className="mx-2">|</div>
-          <Link to={'/Pinterest'} className="mx-2 text-white">용돈 주기 및 이자율 설정</Link>
+          <Link to={"/Pinterest"} className="mx-2 text-white">
+            용돈 주기 및 이자율 설정
+          </Link>
         </div>
       </div>
       {/* <div className="bg-white mx-4 relative rounded-2xl">
@@ -97,58 +95,61 @@ const PchildDetail: React.FC = () => {
       </div> */}
       <div className="flex justify-between mx-4">
         <div className="flex-col w-6/12">
-          <Link to={'/Ploan'}>
+          <Link to={"/Ploan"}>
             <div className="text-black h-2/7 text-xl bg-white rounded-2xl drop-shadow p-3 mb-3 mr-4">
               대출 현황
             </div>
           </Link>
-      <div
-        className="rounded-2xl drop-shadow mb-3 mr-4 h-3/5"
-        style={{
-          backgroundColor: "#FF9FBC",
-        }}
-        // onClick={clickStock}
-      >
-        <div className="text-2xl pt-2 pl-3 text-start text-gray-700">
-          신용 등급
-        </div>
-        <div>
-          <div className="pl-3 text-start text-gray-700">
-            신용등급은?
+          <div
+            className="rounded-2xl drop-shadow mb-3 mr-4 h-3/5"
+            style={{
+              backgroundColor: "#FF9FBC",
+            }}
+            // onClick={clickStock}
+          >
+            <div className="text-2xl pt-2 pl-3 text-start text-gray-700">
+              신용 등급
+            </div>
+            <div>
+              <div className="pl-3 text-start text-gray-700">신용등급은?</div>
+              <div className="px-2 pb-2 text-end flex justify-end items-center">
+                <div className="text-gray-700 text-4xl mr-2">5</div>
+                <div className="text-gray-700 text-2xl">등급</div>
+              </div>
+            </div>
           </div>
-          <div className="px-2 pb-2 text-end flex justify-end items-center">
-            <div className="text-gray-700 text-4xl mr-2">5</div>
-            <div className="text-gray-700 text-2xl">등급</div>
-          </div>
         </div>
-      </div>
-
-      </div>
-      <div className="flex-row w-6/12 mb-6">
-      <Link to={'/Pdeposit'}>    
-      <div
-        className="rounded-2xl drop-shadow mb-3 h-3/5"
-        style={{
-          backgroundColor: "#93C4FF",
-        }}
-        // onClick={clickStock}
-      >
-        <div className="text-2xl pt-2 pl-3 text-start text-gray-700">
-          예금 내역
-        </div>
-          <div className="px-3 pb-2 text-end flex justify-end items-center">
-            <div className="text-gray-700 text-4xl mr-3">3</div>
-            <div className="text-gray-700 text-2xl">건</div>
-            <div>화살표</div>
-        </div>
-        </div>
-      </Link>
+        <div className="flex-row w-6/12 mb-6">
+          <Link to={"/Pdeposit"}>
+            <div
+              className="rounded-2xl drop-shadow mb-3 h-3/5"
+              style={{
+                backgroundColor: "#93C4FF",
+              }}
+              // onClick={clickStock}
+            >
+              <div className="text-2xl pt-2 pl-3 text-start text-gray-700">
+                예금 내역
+              </div>
+              <div className="px-3 pb-2 text-end flex justify-end items-center">
+                <div className="text-gray-700 text-4xl mr-3">3</div>
+                <div className="text-gray-700 text-2xl">건</div>
+                <div>화살표</div>
+              </div>
+            </div>
+          </Link>
           <div className="flex h-2/5">
-            <Link to={'/Plotto'} className="text-black w-6/12 text-2xl bg-white p-3 mr-3 rounded-2xl drop-shadow flex justify-center items-center">
+            <Link
+              to={"/Plotto"}
+              className="text-black w-6/12 text-2xl bg-white p-3 mr-3 rounded-2xl drop-shadow flex justify-center items-center"
+            >
               <div>복권</div>
             </Link>
-            <Link to={`/Pquiz`} className=" text-black w-6/12 text-2xl bg-white p-3 rounded-2xl drop-shadow flex justify-center items-center">
-                <div>퀴즈</div>
+            <Link
+              to={`/Pquiz`}
+              className=" text-black w-6/12 text-2xl bg-white p-3 rounded-2xl drop-shadow flex justify-center items-center"
+            >
+              <div>퀴즈</div>
             </Link>
           </div>
         </div>
@@ -158,40 +159,40 @@ const PchildDetail: React.FC = () => {
           <div>아르바이트 현황 확인하기</div>
         </div>
       </Link>
-      
+
       {isPaneOpen && (
-        <div 
+        <div
           style={{
-            position:'fixed',
-            top :0,
-            left :0,
-            width :'100%',
-            height :'100%',
-            backgroundColor :'rgba(0,0,0,.5)',
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            backgroundColor: "rgba(0,0,0,.5)",
           }}
           // 이거 있으면 오버레이 배경 클릭하면 모달 닫침
           // onClick={() => setIsPaneOpen(false)}
         >
-        <div 
-          style={{
-            transform : isPaneOpen ? 'translateY(0)' : 'translateY(100%)',
-            transition :'transform 2s ease-in-out',
-            position :'fixed',
-            bottom :0,
-            left :0,
-            width :'100%',
-            height :'50%', 
-            backgroundColor:'#fff', 
-          }}
-          onClick={(e) => e.stopPropagation()}
-        >
+          <div
+            style={{
+              transform: isPaneOpen ? "translateY(0)" : "translateY(100%)",
+              transition: "transform 2s ease-in-out",
+              position: "fixed",
+              bottom: 0,
+              left: 0,
+              width: "100%",
+              height: "50%",
+              backgroundColor: "#fff",
+            }}
+            onClick={(e) => e.stopPropagation()}
+          >
             <p>용돈 보내기</p>
             <p>복권</p>
             <p>퀴즈</p>
-          <button onClick={() => setIsPaneOpen(false)}>Close</button>
+            <button onClick={() => setIsPaneOpen(false)}>Close</button>
+          </div>
         </div>
-      </div>  
-    )}
+      )}
       {/* <PMenuList /> */}
     </div>
   );
