a4e173ebf57544582b86acdfbc204612ed4eb3c8
un2642 <un2642@naver.com>
2023-09-25 15:09:39 +0900

    💄[FE]Design : 가계부쓰기 라디오버튼 수정

--- Frontend/src/components/Header/Header.tsx
+++ Frontend/src/components/Header/Header.tsx
@@ -98,14 +98,14 @@ const Header: React.FC = () => {
       <nav className="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <div className="flex justify-center items-center">
-              <div
-                onClick={goBack}
-                className="inline-flex items-center justify-center p-2 w-11 h-11"
-              >
-                <img src="../../../src/assets/main/back.png" />
-              </div>
+            <div
+              onClick={goBack}
+              className="inline-flex items-center justify-center p-2 w-11 h-11"
+            >
+              <img src="../../../src/assets/main/back.png" />
+            </div>
 
-            <div className="p-2">{now}</div>
+            <div className="p-2 text-xl">{now}</div>
           </div>
           <div className="flex w-2/6 justify-end">
             <div className="p-2 w-11 h-11">
