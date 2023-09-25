0b3df622b36e8695b4170a124017dc2c4cae954d
un2642 <un2642@naver.com>
2023-09-25 16:01:23 +0900

    💄[FE]Design: 대출 progress바

--- Frontend/src/components/Loan/LoanStatus.tsx
+++ Frontend/src/components/Loan/LoanStatus.tsx
@@ -1,32 +1,32 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode } from "react";
 
 interface LoanStatusProps {
-    children: ReactNode;
-  }
+  children: ReactNode;
+}
 
 const LoanStatus: React.FC<LoanStatusProps> = (props) => {
-    return (
-        <div className="border border-2 rounded-xl mr-6 ml-6 bg-white" style={{borderColor: '#ABD0CE'}}>
-        <h2 className='mt-2 mb-2'>규렬님 대출 현황</h2>
-        <div className='flex justify-between'>
-            <p className='flex ml-4'>대출 보유 개수</p>
-            <p className='flex mr-4'>2건</p>
-        </div>
-        <div className='flex justify-between'>
-            <p className='flex ml-4'>총 대출 잔액</p>
-            <p className='flex mr-4'>10,000원</p>
-        </div>
-        <div className='flex justify-between'>
-            <p className='flex ml-4'>이자</p>
-            <p className='flex mr-4'>100원</p>
-        </div>
-        <div className='flex justify-between'>
-            <p className='flex ml-4'>대출 한도</p>
-        </div>
-            <div className='border'>그래프</div>
-        {props.children}
-        </div>
-    )
+  return (
+    <div className="border rounded-xl mr-6 ml-6 bg-white text-xl mt-3 drop-shadow-md">
+      <h2 className="mt-2 mb-2 text-2xl p">규렬님 대출 현황</h2>
+      <div className="flex justify-between">
+        <p className="flex ml-4">대출 보유 개수</p>
+        <p className="flex mr-4">2건</p>
+      </div>
+      <div className="flex justify-between">
+        <p className="flex ml-4">총 대출 잔액</p>
+        <p className="flex mr-4">10,000원</p>
+      </div>
+      <div className="flex justify-between">
+        <p className="flex ml-4">이자</p>
+        <p className="flex mr-4">100원</p>
+      </div>
+      <div className="flex justify-between">
+        <p className="flex ml-4">대출 한도</p>
+      </div>
+      <div className="border">그래프</div>
+      {props.children}
+    </div>
+  );
 };
 
 export default LoanStatus;
