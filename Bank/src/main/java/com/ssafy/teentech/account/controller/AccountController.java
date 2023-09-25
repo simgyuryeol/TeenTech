8c51fa53b99202ffbd44c37c1cecfa21e7607f7c
hsj4436 <hsj4436@gmail.com>
2023-09-25 14:54:31 +0900

    :recycle: [BE]Refactor: 리턴 타입 변경

--- Bank/src/main/java/com/ssafy/teentech/account/controller/AccountController.java
+++ Bank/src/main/java/com/ssafy/teentech/account/controller/AccountController.java
@@ -2,9 +2,7 @@ package com.ssafy.teentech.account.controller;
 
 import com.ssafy.teentech.account.dto.AccountResponseDto;
 import com.ssafy.teentech.account.service.AccountService;
-import com.ssafy.teentech.common.ApiResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,15 +17,10 @@ public class AccountController {
     private final AccountService accountService;
 
     @GetMapping("/information")
-    public ResponseEntity<ApiResponse> getAccountInformation(@RequestParam Long userId) {
+    public ResponseEntity<AccountResponseDto> getAccountInformation(@RequestParam Long userId) {
         AccountResponseDto accountResponseDto = accountService.getAccountInformation(userId);
 
-        ApiResponse apiResponse = ApiResponse.builder()
-            .message("계좌 정보 조회 완료")
-            .status(HttpStatus.OK.value())
-            .data(accountResponseDto)
-            .build();
-        return ResponseEntity.ok(apiResponse);
+        return ResponseEntity.ok(accountResponseDto);
     }
 
 }
