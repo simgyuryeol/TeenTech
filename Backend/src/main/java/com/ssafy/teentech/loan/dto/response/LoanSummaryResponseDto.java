f3481f23795c12fa596f5d158626fefb6525f8ad
hsj4436 <hsj4436@gmail.com>
2023-09-25 10:31:48 +0900

    :recycle: [BE]Refactor: 쿼리에 타입캐스트 제거 및 dto 생성자 추가

--- Backend/src/main/java/com/ssafy/teentech/loan/dto/response/LoanSummaryResponseDto.java
+++ Backend/src/main/java/com/ssafy/teentech/loan/dto/response/LoanSummaryResponseDto.java
@@ -1,9 +1,11 @@
 package com.ssafy.teentech.loan.dto.response;
 
 import java.time.LocalDate;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class LoanSummaryResponseDto {
 
     private Long loanId;
