6202c779b7b0e323cd66c9eb8c8af1338426c120
hsj4436 <hsj4436@gmail.com>
2023-09-25 09:35:32 +0900

    :sparkles: [BE]Feat: dto 작성
    대출 신청 요청
    신청된 대출 응답
    대출 승인 요청
    대출 내역 응답
    대출 거절 요청
    부모가 조회하는 대출 요약 응답
    대출 요약 응답
    상환 요청

---
+++ Backend/src/main/java/com/ssafy/teentech/loan/dto/request/LoanRejectRequestDto.java
@@ -0,0 +1,12 @@
+package com.ssafy.teentech.loan.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LoanRejectRequestDto {
+
+    private Long loanId;
+
+}
