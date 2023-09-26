603270b63d972839175f4ff399daefc6582b5267
hsj4436 <hsj4436@gmail.com>
2023-09-26 10:16:59 +0900

    :recycle: [BE]Refactor: 허용 헤더 변경

--- Backend/src/main/java/com/ssafy/teentech/common/config/WebConfig.java
+++ Backend/src/main/java/com/ssafy/teentech/common/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
             .allowedOrigins("http://localhost:3000", "http://localhost:5173")
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-            .allowedHeaders("*")
+            .allowedHeaders("Authorization")
             .allowCredentials(true)
             .maxAge(3600);
     }
