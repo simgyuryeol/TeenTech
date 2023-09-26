9821e9ad04a2d4696f7f9c8155f5860c523186aa
simgyuryeol <tlarbfuf@gmail.com>
2023-09-27 08:43:36 +0900

    ✨ [BE] Feat: 매주,매월 용돈이체, 매일 예금만료 개발

---
+++ Backend/src/main/java/com/ssafy/teentech/scheduler/DepositExpiration.java
@@ -0,0 +1,50 @@
+package com.ssafy.teentech.scheduler;
+
+import com.ssafy.teentech.bank.service.BankService;
+import com.ssafy.teentech.deposit.domain.Deposit;
+import com.ssafy.teentech.deposit.repository.DepositRepository;
+import com.ssafy.teentech.deposit.service.DepositService;
+import com.ssafy.teentech.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class DepositExpiration {
+    private final BankService bankService;
+    private final DepositRepository depositRepository;
+    private final DepositService depositService;
+
+    @Scheduled(cron = "0 0 9 * * *") // 매일
+    public void depositExpiration(){
+        ZonedDateTime nowDate = ZonedDateTime.now(ZoneId.of("Asia/Seoul")); //서울 오늘 날짜
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String formattedDate = nowDate.format(formatter);
+
+
+        List<Deposit> all = depositRepository.findAll();
+
+        for (Deposit deposit : all) {
+            // 두 날짜를 비교합니다.
+            int comparisonResult = deposit.getEndDate().compareTo(LocalDate.parse(formattedDate, formatter));
+
+            if(comparisonResult<=0){ //같거나 이후 날짜면 예금 만료 된 것으로 판단하여 이체 실행한다.
+                /**
+                 * 1. 자식에게 예금을 이체해준다.
+                 * 2. 예금을 삭제한다.
+                 */
+                depositService.depositExpiration(deposit.getUser().getUserId(), deposit.getDepositId());
+            }
+        }
+
+
+    }
+}
