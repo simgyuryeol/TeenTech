c94de0921e799991826cdf3cba790ad8a4b95e8e
un2642 <un2642@naver.com>
2023-09-26 14:58:56 +0900

    ♻️ [FE]Refactor: 이미지 import 수정

--- Frontend/dev-dist/sw.js
+++ Frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.1g3n273t3ho"
+    "revision": "0.gq802cd52e"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
