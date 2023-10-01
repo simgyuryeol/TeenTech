337f28746b7f5c592bd0a656eb57c9e776055e9d
yoon-b <eunbi.kim.kor@gmail.com>
2023-10-01 11:37:29 +0900

    ✨ [FE]Feat: 주식 뉴스 가져오기

--- Frontend/src/pages/Child/Stock/Stock.tsx
+++ Frontend/src/pages/Child/Stock/Stock.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import StockPortfolio from "../../../components/Stock/StockPortfolio";
 import MyStock from "../../../components/Stock/MyStock";
@@ -23,6 +24,7 @@ interface Stock {
 
 const Stock: React.FC = () => {
   const navigator = useNavigate();
+  const [myStocks, setMystocks] = useState([]);
 
   const [stepsEnabled, setStepsEnabled] = useState(false);
   const [initialStep] = useState(0);
@@ -30,6 +32,19 @@ const Stock: React.FC = () => {
     options: tourOptions,
     steps: tourSteps,
   });
+
+  useEffect(() => {
+    axios
+      // .get(import.meta.env.VITE_BASE_URL + `${child_id}/investments`, {
+      .get(import.meta.env.VITE_BASE_URL + "/34/investments")
+      .then((response) => {
+        console.log(response.data);
+        setMystocks(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   const onExit = () => {
     setStepsEnabled(false);
@@ -43,25 +58,6 @@ const Stock: React.FC = () => {
     navigator("/StockMarket");
   };
 
-  const sampleData: Stock[] = [
-    {
-      koName: "삼성전자",
-      enName: "samsung",
-      investment: 1000,
-      value: 900,
-      gain: -100,
-      ror: -10.0,
-    },
-    {
-      koName: "카카오",
-      enName: "kakao",
-      investment: 1000,
-      value: 1100,
-      gain: 100,
-      ror: 10.0,
-    },
-  ];
-
   return (
     <React.Fragment>
       <Steps
@@ -73,7 +69,7 @@ const Stock: React.FC = () => {
       />
 
       <div className="mt-16">
-        <div className="p-3"/>
+        <div className="p-3" />
         <div className="flex justify-end mr-4">
           <Icon
             icon="mdi:help-circle-outline"
@@ -88,13 +84,24 @@ const Stock: React.FC = () => {
           <p className="font-bold text-2xl text-left mt-4 mx-8">
             나의 주식 살펴보기
           </p>
-          {sampleData.length ? (
-            sampleData.map((stock, index) => (
+          {myStocks.length ? (
+            myStocks.map((stock, index) => (
               <MyStock key={index} stock={stock} />
             ))
           ) : (
             <EmptyStock />
           )}
+          
+          <MyStock
+            stock={{
+              koName: "삼성전자",
+              enName: "samsung",
+              investment: 1000,
+              value: 900,
+              gain: -100,
+              ror: -10.0,
+            }}
+          />
         </div>
 
         <button
