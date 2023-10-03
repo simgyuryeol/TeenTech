51e0401ec6451bcfcdb33bddedc35ef901a45043
정재범 <zxsefv@gmail.com>
2023-10-03 18:49:47 +0900

    :sparkles: [FE]Feat: 액세스토큰 저장 변경

--- Frontend/src/components/Login/Role.tsx
+++ Frontend/src/components/Login/Role.tsx
@@ -60,7 +60,7 @@ interface RoleProps {
               })
               .then(response => {
                 console.log(response.data.data)
-                window.localStorage.setItem('accessToken', response.data.data);
+                window.localStorage.setItem('accessToken', response.data.data.accessToken);
                 if (role === '"ROLE_PARENT"'){
                   navigate('../pmain')
                 }
