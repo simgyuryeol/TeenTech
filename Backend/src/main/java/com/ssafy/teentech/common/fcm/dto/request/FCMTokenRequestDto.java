815e203225e5a56a9b49e7fdc49f0206e595be63
hsj4436 <hsj4436@gmail.com>
2023-10-04 10:43:45 +0900

    :bug: [BE]Fix: AllArgsConstructor 어노테이션 삭제

--- Backend/src/main/java/com/ssafy/teentech/common/fcm/dto/request/FCMTokenRequestDto.java
+++ Backend/src/main/java/com/ssafy/teentech/common/fcm/dto/request/FCMTokenRequestDto.java
@@ -1,10 +1,8 @@
 package com.ssafy.teentech.common.fcm.dto.request;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class FCMTokenRequestDto {
 
     private String fcmToken;
