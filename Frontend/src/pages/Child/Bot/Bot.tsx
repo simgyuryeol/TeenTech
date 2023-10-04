84a5bcb331e214f278959cdc2a362727b0adfaa3
un2642 <un2642@naver.com>
2023-10-04 09:42:33 +0900

    ♻️ [FE]Refactor : 챗봇 App.tsx 이동

--- Frontend/src/pages/Child/Bot/Bot.tsx
+++ Frontend/src/pages/Child/Bot/Bot.tsx
@@ -10,12 +10,17 @@ const Bot: React.FC = () => {
     setOpen(open === value ? 0 : value);
 
   return (
-    <div className="">
-      <img src={dog} width={"100px"} onClick={() => handleOpen(1)}></img>
-      {open === 1 && <Chatbotmain closeModal={handleOpen}></Chatbotmain>}
-      {open === 1 && (
-        <BotModal closeModal={handleOpen} children={""}></BotModal>
-      )}
+    <div style={{ position: "fixed", bottom: 0, right: 0, zIndex: 9999 }}>
+      <div className="flex items-end">
+        <div className="bg-sky-200 rounded-lg drop-shadow-md p-2 mb-3">
+          질문해줘
+        </div>
+        <img src={dog} width={"100px"} onClick={() => handleOpen(1)}></img>
+        {open === 1 && <Chatbotmain closeModal={handleOpen}></Chatbotmain>}
+        {open === 1 && (
+          <BotModal closeModal={handleOpen} children={""}></BotModal>
+        )}
+      </div>
     </div>
   );
 };
