f4b59a8fbc3a703e5b39dda29d52f648de6edf14
정재범 <zxsefv@gmail.com>
2023-10-05 07:40:43 +0900

    :sparkles: [FE]Fix: 대출 페이지 수정

--- Frontend/dev-dist/sw.js
+++ Frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4hfa67k1f9"
+    "revision": "0.10ve7dff5vo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
