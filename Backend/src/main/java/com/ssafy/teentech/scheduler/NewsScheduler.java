0b48ce17f17a528e3d122d9756a66bb2cabbf1d5
simgyuryeol <tlarbfuf@gmail.com>
2023-10-04 17:22:54 +0900

    ✨ [BE] Feat: 뉴스 시간 임시 변경

--- Backend/src/main/java/com/ssafy/teentech/scheduler/NewsScheduler.java
+++ Backend/src/main/java/com/ssafy/teentech/scheduler/NewsScheduler.java
@@ -46,8 +46,8 @@ public class NewsScheduler {
     private static String apiUrl = "https://api.openai.com/v1/chat/completions";
 //    private static String apiUrl = "https://api.openai.com/v1/engines/text-davinci-003/completions";
 
-    @Scheduled(cron = "0 0 16 * * *") //매일 오후 4시에 실행
-//    @Scheduled(cron = "0 31 * * * *")
+    @Scheduled(cron = "0 30 17 * * *") //매일 오후 4시에 실행
+//    @Scheduled(cron = "0 18 * * * *")
     public void newsSave(){
         List<News> newsList = new ArrayList<>();
 
