ccc69fed8a98ee8d4a2a0c280b18e81e4589b5ba
hsj4436 <hsj4436@gmail.com>
2023-10-04 22:14:59 +0900

    :recycle: [FE]Refactor: 이벤트 리스너 삭제

--- Frontend/public/my-custom-sw.ts
+++ Frontend/public/my-custom-sw.ts
@@ -1,18 +1,14 @@
-
+import { registerRoute } from 'workbox-routing';
+import { NetworkOnly } from 'workbox-strategies';
 
 registerRoute(
   ({url}) => url.href.startsWith('https://j9e207.p.ssafy.io/oauth2/authorization/kakao'),
   new NetworkOnly()
 );
 
-
+import { NetworkFirst } from 'workbox-strategies';
 
 registerRoute(
   ({url}) => url.href.startsWith('https://j9e207.p.ssafy.io/api/v1/'),
   new NetworkFirst()
 );
-
-self.addEventListener('install', function(event) {
-    event.waitUntil(self.skipWaiting());
-  });
-  
