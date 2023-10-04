efd267f86e603f36199c759f92e74d7ac4b10f8f
hsj4436 <hsj4436@gmail.com>
2023-10-04 21:39:27 +0900

    :recycle: [FE]Refactor: 서비스 워커 테스트

--- Frontend/my-custom-sw.js
+++ Frontend/my-custom-sw.js
@@ -12,3 +12,8 @@ registerRoute(
   ({url}) => url.href.startsWith('https://j9e207.p.ssafy.io/api/v1/'),
   new NetworkFirst()
 );
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(self.skipWaiting());
+  });
+  
