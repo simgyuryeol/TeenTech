c38d037087a2d091c4bee7b002f2b38946b94093
hsj4436 <hsj4436@gmail.com>
2023-10-04 22:04:47 +0900

    :recycle: [FE]Refactor: import 변경

--- Frontend/public/my-custom-sw.js
+++ Frontend/public/my-custom-sw.js
@@ -1,12 +1,11 @@
-import { registerRoute } from 'workbox-routing';
-import { NetworkOnly } from 'workbox-strategies';
+
 
 registerRoute(
   ({url}) => url.href.startsWith('https://j9e207.p.ssafy.io/oauth2/authorization/kakao'),
   new NetworkOnly()
 );
 
-import { NetworkFirst } from 'workbox-strategies';
+
 
 registerRoute(
   ({url}) => url.href.startsWith('https://j9e207.p.ssafy.io/api/v1/'),
--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
+import { registerRoute } from 'workbox-routing';
+import { NetworkOnly } from 'workbox-strategies';
+import { NetworkFirst } from 'workbox-strategies';
 
 // https://vitejs.dev/config/
 export default defineConfig({
