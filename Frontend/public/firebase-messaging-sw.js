813f07af4e160b2c4879f2ebcf1d1f02b7146caf
yoon-b <eunbi.kim.kor@gmail.com>
2023-10-05 23:37:38 +0900

    💩 [FE]Poo: fcm 수정중

--- Frontend/public/firebase-messaging-sw.js
+++ Frontend/public/firebase-messaging-sw.js
@@ -1,36 +1,33 @@
-self.addEventListener("install", function (e) {
+self.addEventListener("install", function(e) {
   console.log("fcm sw install..");
   self.skipWaiting();
 });
 
-self.addEventListener("activate", function (e) {
+self.addEventListener("activate", function(e) {
   console.log("fcm sw activate..");
 });
 
-self.addEventListener("push", function (e) {
+self.addEventListener("push", function(e) {
   if (!e.data.json()) return;
-  
+
   const resultData = e.data.json();
   console.log("알림 정보: ", resultData.notification);
   console.log("알림 제목: ", resultData.notification.title);
   console.log("알림 내용: ", resultData.notification.body);
-  // const notificationTitle = resultData.notification.title;
 
-  // const notificationOptions = {
-  //   body: resultData.notification.body,
-    //     body: resultData.body,
-    //     icon: resultData.image,
-    //     tag: resultData.tag,
-    //     ...resultData,
-  // };
+  const notificationTitle = resultData.notification.title;
+  const notificationOptions = {
+    body: resultData.notification.body,
+    icon: "/moneybox-192.png",
+  };
 
   // console.log("push: ", { resultData, notificationTitle, notificationOptions });
-  //   console.log("push: ", { notificationTitle, notificationOptions });
+  // console.log("push: ", { notificationTitle, notificationOptions });
 
-  // self.registration.showNotification(notificationTitle, notificationOptions);
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
-self.addEventListener("notificationclick", function (event) {
+self.addEventListener("notificationclick", function(event) {
   console.log("notification click");
   const url = "/";
   event.notification.close();
